0d0354353858f820a932836e7521c5a3aaef3b1a
umeshsobti <itika@trigvent.com>
2024-05-31 11:04:44 +0530

    used routing in 2 different forms

--- basic_react/src/App.js
+++ basic_react/src/App.js
@@ -1,16 +1,34 @@
 import Form from "./components/Form.js";
 import FormFormik from "./components/FormikForm.js";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Form style={{
-      margin: "0",
-      padding: "0",
-      boxSizing: "border-box"
-  }}/>
-
-  {/* <FormFormik/> */}
-    </div>
+      <Router>
+        <main>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/form">Simple Form Validations</Link>
+              </li>
+              <li>
+                <Link to="/formik">Formik Form</Link>
+              </li>
+            </ul>
+          </nav>
+        
+        </main>
+        <Routes>
+          <Route
+            path="/form"
+            element={
+              <Form
+                
+              />
+            }
+          ></Route>
+          <Route path="/formik" element={<FormFormik />}></Route>
+        </Routes>
+      </Router>
   );
 }
 export default App;
